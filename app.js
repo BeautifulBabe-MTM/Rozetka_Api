b2b2b6fe49e04910ec5d292ad4b47d044c225b23
BeautifulBabe-MTM <86706552+BeautifulBabe-MTM@users.noreply.github.com>
2022-12-22 11:11:07 +0200

    adding cards

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+//olx token - WfDdwgiDQwfRx3GByO1B8rNUgJ1VP99DFJGMlzAeFVt13PFc
+
 const data = document.getElementById('product_input');
-const url = 'https://api-seller.rozetka.com.ua/items/search'
+const url = 'https://rozetka.com.ua/promo/newyear/?gclid=Cj0KCQiA-oqdBhDfARIsAO0TrGGLGdEhTk-pK0tfEMtQHEqARKxlfJZgsd_D3QHTRO2jRgszzeBnE_kaAs-iEALw_wcB'
 
 async function postData(){
     const response = await fetch(url, 
         { 
             method: "GET", 
-            mode: 'cors', 
+            mode: 'cors',   
             headers: { 'Content-Type': 'application/json'}
         })
         .then(data => data.json()).then(data => console.log(data));
